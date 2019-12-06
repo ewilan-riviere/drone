a5021d6e79b9ee9f82b4d3e36ba7af25c3cca03d
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2019-12-06 18:06:20 +0100

    add autodeploy prod support

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Useweb Drone
 
-> Outil de déploiement de code sur le serveur courant.
+> Outil de déploiement de code.
 
 ## Features
 
-Se contente de réceptionner le colis (payload), via les git webhooks puis d'effectuer la livraison, en lançant un `git pull` sur les différents répertoires dédiés au projet, d'où son nom ;)
+Se contente de réceptionner le colis (payload) via les git webhooks puis d'effectuer la livraison, en lançant un `git pull` sur les différents répertoires dédiés aux projets.
+Permet également de mettre à jour par action manuelle différents projets sur différents serveurs, couramment ceux de production.
 
 ## Setup
 
+### Autodeploy via Git Hooks
+
 Configurer les variables d'environnements suivants :
 
-* WEBHOOK_PATH : Chemin HTTP d'écoute du payload, à la racine par défaut.
+* WEBHOOK_PATH : Chemin HTTP d'écoute du payload des git hooks, sur /deploy par défaut.
 * PROJECTS_ROOT : Répertoire racine contenant la liste des différents projets clonés.
 
 Par défaut le git pull s'effectue sur un nom de répertoire semblable au nom slugifié du répository.
@@ -25,5 +28,41 @@ Il est tout a fait possible de configurer pour chaque projet un ou plusieurs ré
      "nom-du-repo-2-dev",
      "nom-du-repo-2-rec"
   ]
+}
+```
+
+### Autodeploy via action manuelle
+
+Configurer les variables d'environnements suivants :
+
+* WEBPULL_PATH : Chemin HTTP pour la demande de mise à jour, sur /prod par défaut.
+* PULL_KEY : Clé de protection à transmettre dans la requête POST de demande de mise à jour.
+* SSH_KEY : Clé SSH privée de l'utilisateur courant.
+
+Voici le format de body à utiliser dans l'appel POST vers le endpoint :
+
+```json
+{
+  "name": "nomprojet",
+  "key": "macle"
+}
+```
+
+Le git pull s'effectuera sur l'ensemble des serveurs "nodes" indiqués pour chque projet.
+Pour configurer la liste des serveurs par projet, créer un fichier `servers.json` à la racine. Un format d'example du fichier :
+
+```json
+{
+  "nomprojet": {
+    // Utilisateur pour la connection SSH
+    "username": "useweb",
+    // Liste des serveurs "node" à mettre à jour
+    "servers": ["laforet-prod-web1.useweb.net", "laforet-prod-web2.useweb.net"],
+    // Liste des répertoires à mettre à jour
+    "dirs": [
+      "/var/www/laforet-back",
+      "/var/www/laforet-front"
+    ]
+  }
 }
 ```
--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 require('dotenv').config();
 const slugify = require('slugify');
 const fs = require('fs');
+const Client = require('ssh2').Client;
 
 const port = parseInt(process.env.PORT || '3000', 10);
 
-let repositories = JSON.parse(fs.readFileSync('repositories.json'));
-
 let express = require('express'),
-    http = require('http'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    childprocess = require('child_process'),
-    path = require('path');
+  http = require('http'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  childprocess = require('child_process'),
+  path = require('path');
 
 app.use(bodyParser.json());
 
-app.post(process.env.WEBHOOK_PATH || '/', (req, res) => {
+app.post(process.env.WEBHOOK_PATH || '/deploy', (req, res) => {
   if (!req.body || !req.body.repository || !req.body.repository.name) {
-    return res.status(400).json({ message: 'Invalid request!' });
+    return res.status(400).json({
+      message: 'Invalid request!'
+    });
   }
 
-  let name = slugify(req.body.repository.name, { lower: true });
+  let name = slugify(req.body.repository.name, {
+    lower: true
+  });
 
   let dirs = [name];
 
+  let repositories = JSON.parse(fs.readFileSync('repositories.json'));
   if (repositories[name]) {
     dirs = repositories[name];
   }
 
-  dirs.forEach(function(dirname) {
+  dirs.forEach(function (dirname) {
     let projectDir = path.normalize(process.env.PROJECTS_ROOT + dirname);
 
     childprocess.exec(`cd ${projectDir} && git pull`);
@@ -37,7 +41,58 @@ app.post(process.env.WEBHOOK_PATH || '/', (req, res) => {
     console.log(`${projectDir} pulled at ${date.toString()} !`);
   });
 
-  res.status(200).json({ message: 'Git Hook received!' });
+  res.status(200).json({
+    message: 'Git Hook received!'
+  });
+});
+
+app.post(process.env.WEBPULL_PATH || '/prod', (req, res) => {
+  if (!req.body || !req.body.name || !req.body.key) {
+    return res.status(400).json({
+      message: 'Invalid request!'
+    });
+  }
+
+  if (req.body.key !== process.env.PULL_KEY) {
+    return res.status(400).json({
+      message: 'Invalid key!'
+    });
+  }
+
+  let projects = JSON.parse(fs.readFileSync('projects.json'));
+  let project = projects[req.body.name]
+
+  if (!project) {
+    return res.status(400).json({
+      message: 'Invalid project!'
+    });
+  }
+
+  project.servers.forEach(function (server) {
+    var conn = new Client();
+    conn.on('ready', function () {
+      project.dirs.forEach(function (dir) {
+        conn.exec(`cd ${dir} && git pull`, function (err, stream) {
+          if (err) throw err;
+          stream.on('data', function (data) {
+            console.log('OUTPUT: ' + data);
+          });
+        });
+      });
+    }).connect({
+      host: server,
+      port: 22,
+      username: project.username,
+      privateKey: fs.readFileSync(process.env.SSH_KEY)
+    });
+  });
+
+  const date = new Date();
+  console.log(`${req.body.name} pulled at ${date.toString()} !`);
+
+  res.status(200).json({
+    message: 'Git Pull received!'
+  });
 });
 
 http.createServer(app).listen(port, () => {
