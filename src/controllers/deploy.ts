d7f3c1c838ee76bc19fd8d624b39ad5d9cb78e99
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-06-27 19:02:31 +0200

    feat: Add GitPayload interface and update deploy controller

--- src/controllers/deploy.ts
+++ src/controllers/deploy.ts
@@ -1,9 +1,27 @@
 import type { H3Event } from 'h3'
 import { readBody } from 'h3'
 
+interface GitPayload {
+  ref: string
+  repository: {
+    name: string
+    full_name: string
+  }
+  pusher: {
+    name: string
+  }
+
+}
+
 export default async (event: H3Event) => {
-  const body = await readBody(event)
-  console.log(body)
+  const body = await readBody<GitPayload | undefined>(event)
+  if (!body) {
+    return {
+      message: 'No body',
+    }
+  }
+
+  console.log(body.pusher.name)
 
   return {
     message: 'Deploy',
