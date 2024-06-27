454cb03424d64234399af424bfbc41e54896bceb
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-06-27 19:31:05 +0200

    Remove unnecessary console.log statements and update deploy controller

--- src/controllers/deploy.ts
+++ src/controllers/deploy.ts
@@ -13,7 +13,7 @@ export default async (event: H3Event) => {
   const host = event.headers.get('host')
   console.log(host)
   console.log(body.repository.name)
-  console.log(event.node.req)
+  console.log(event.headers)
 
   if (event.node.req.url) {
     const requestUrl = new URL(event.node.req.url)
