2c48f6e9a2860652eb87c9b22fe1622b2f681af1
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-06-27 19:43:18 +0200

    chore: Remove unnecessary console.log statements and update deploy controller

--- src/controllers/deploy.ts
+++ src/controllers/deploy.ts
@@ -1,13 +1,16 @@
 import type { H3Event } from 'h3'
-import { readBody } from 'h3'
+import { createError, readBody } from 'h3'
 import type { GithubPayload, GitlabPayload } from '@/types'
 
 export default async (event: H3Event) => {
   const body = await readBody<GithubPayload | GitlabPayload | undefined>(event)
-  if (!body) {
-    return {
-      message: 'No body',
-    }
+  if (!body || !body.repository || !body.repository.name) {
+    throw createError({
+      status: 400,
+      statusMessage: 'Bad Request',
+      message: 'Invalid body',
+      data: !body ? 'No body' : !body.repository ? 'No repository' : 'No repository name',
+    })
   }
 
   const userAgent = event.headers.get('user-agent')
